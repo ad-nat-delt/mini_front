39b2a598316ab4ae8e4cb5fa0e6939070cd876e1
adithya d <adithya.d.kochu@gmail.com>
2024-04-14 01:15:07 +0530

    Added page details

--- components/homeScreen.js
+++ components/homeScreen.js
@@ -1,50 +1,125 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity,Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+
+
+
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to MyApp</Text>
-      <Text style={styles.subtitle}>Your one-stop solution for all your needs</Text>
+      <Text style={styles.head}>
+        EavesDropper
+      </Text>
+      <Image
+        source={require('../assets/convo.png')}
+        style={styles.image}
+      />
+      <Text style={styles.title}>
+        A platform to record your day to day conversations and provide results based on it
+      </Text>
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Record')}
       >
         <Text style={styles.buttonText}>Get Started</Text>
       </TouchableOpacity>
-      <StatusBar style="auto" />
+      <Text style={styles.caption}>Tap to get started.</Text>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF694',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
-  title: {
-    fontSize: 24,
+  head:{
+    marginTop: 0,
     fontWeight: 'bold',
-    marginBottom: 10,
+    fontSize: 45,
+    marginBottom: 80,
+    fontStyle: 'italic',
+    textAlign: 'center',
   },
-  subtitle: {
-    fontSize: 16,
-    color: '#666',
+  image: {
+    width: 231,
+    height: 218,
     marginBottom: 20,
   },
+  title: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 20,
+    color: '#4D2828'
+  },
   button: {
-    backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 5,
+    backgroundColor: '#D67638',
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    marginTop: 20
   },
   buttonText: {
-    color: '#fff',
+    color: 'black',
     fontWeight: 'bold',
+    fontSize: 19,
+  },
+  caption: {
+    marginTop: 50,
+    color: '#4D2828',
     fontSize: 18,
   },
 });
+
+
+
+// const HomeScreen = ({ navigation }) => {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Welcome to MyApp</Text>
+//       <Text style={styles.subtitle}>Your one-stop solution for all your needs</Text>
+//       <TouchableOpacity
+//         style={styles.button}
+//         onPress={() => navigation.navigate('Record')}
+//       >
+//         <Text style={styles.buttonText}>Get Started</Text>
+//       </TouchableOpacity>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// };
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//     marginBottom: 10,
+//   },
+//   subtitle: {
+//     fontSize: 16,
+//     color: '#666',
+//     marginBottom: 20,
+//   },
+//   button: {
+//     backgroundColor: 'blue',
+//     padding: 10,
+//     borderRadius: 5,
+//   },
+//   buttonText: {
+//     color: '#fff',
+//     fontWeight: 'bold',
+//     fontSize: 18,
+//   },
+// });
 
 export default HomeScreen;
--- components/recordAudio.js
+++ components/recordAudio.js
@@ -4,116 +4,168 @@ import { Audio } from 'expo-av';
 import { useNavigation } from '@react-navigation/native';
 
 const RecordScreen = () => {
-    const [recording, setRecording] = useState();
-    const [permissionResponse, requestPermission] = Audio.usePermissions();
-    const [sound, setSound] = useState();
-    const [isPlaying, setIsPlaying] = useState();
-    const navigation = useNavigation();
-  
-    async function startRecording() {
-      try {
-        if (permissionResponse.status !== 'granted') {
-          console.log('Requesting permission..');
-          await requestPermission();
-        }
-        await Audio.setAudioModeAsync({
-          allowsRecordingIOS: true,
-          playsInSilentModeIOS: true,
-        });
-  
-        console.log('Starting recording..');
-        const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY
-        );
-        setRecording(recording);
-        console.log('Recording started');
-      } catch (err) {
-        console.error('Failed to start recording', err);
+  const [recording, setRecording] = useState();
+  const [play, setPlay]=useState(false);
+  const [permissionResponse, requestPermission] = Audio.usePermissions();
+  const [sound, setSound] = useState();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [shouldPlayAfterRecording, setShouldPlayAfterRecording] = useState(false);
+  const navigation = useNavigation();
+  const [uri, setURI] = useState(null); 
+
+  async function startRecording() {
+    
+    try {
+      
+      setShouldPlayAfterRecording(false);
+      if (permissionResponse.status !== 'granted') {
+        console.log('Requesting permission..');
+        await requestPermission();
       }
-    }
-  
-    async function stopRecording() {
-      console.log('Stopping recording..');
-      setRecording(undefined);
-      await recording.stopAndUnloadAsync();
-      await Audio.setAudioModeAsync(
-        {
-          allowsRecordingIOS: false,
-        }
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY
       );
-      const uri = recording.getURI();    
-      console.log('Recording stopped and stored at', uri);
-      playSound(uri);
-      navigation.navigate('Functions');
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
     }
-    async function playSound(uri) {
-  
+  }
+
+  async function stopRecording() {
+    try {
+      setPlay(true);
+      console.log('Stopping recording..');
+      await recording.stopAndUnloadAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+      });
+      const uri = recording.getURI();
+      setURI(uri); // Set uri globally
+      setRecording(undefined);
+      console.log('Recording stopped and stored at', uri);
+    } catch (err) {
+      console.error('Failed to stop recording', err);
+    }
+  }
+
+  async function playSound(uri) {
+    try {
       console.log('Loading Sound');
       const { sound } = await Audio.Sound.createAsync({ uri });
-      //const { sound } = await Audio.Sound.createAsync( require('./files/hello.mp3'));
-      setIsPlaying(true); 
       setSound(sound);
-      
   
       console.log('Playing Sound');
-      setIsPlaying(false); 
       await sound.playAsync();
-      
+      setIsPlaying(true);
+    } catch (error) {
+      console.error('Error playing sound:', error);
     }
-    async function stopSound() {
+  }
+  
+  async function stopSound() {
+    try {
       if (sound) {
         console.log('Stopping Sound');
         await sound.stopAsync();
-        setIsPlaying(false); 
+        setIsPlaying(false);
       }
+    } catch (error) {
+      console.error('Error stopping sound:', error);
     }
-  
-    useEffect(() => {
-      return sound
-        ? () => {
-            console.log('Unloading Sound');
-            sound.unloadAsync();
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={async () => {
+          if(!recording)
+          {
+            setPlay(false);
+            startRecording();
           }
-        : undefined;
-    }, [sound]);
-  
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity
-            style={styles.button}
-            onPress={recording ? stopRecording : startRecording}
-            >
-            <Text style={styles.buttonText}>
-                {recording ? 'Stop Recording' : 'Start Recording'}
-            </Text>
+          else
+          {
+            stopRecording()
+          }
+       }}
 
+      >
+        <Text style={styles.buttonText}>
+          {recording ? 'Stop Recording' : 'Start Recording'}
+        </Text>
       </TouchableOpacity>
-        {isPlaying && (
-            <Button
-                title="Stop Playback"
-                onPress={stopSound}
-            />
-        )}
+      {play && (
+        <View>
+          <TouchableOpacity 
+          style={styles.button}           
+            onPress={() => {
+              setShouldPlayAfterRecording(true);
+              playSound(uri);
+            }}
+            
+          ><Text style={styles.buttonText}>Replay</Text>
+          </TouchableOpacity>
 
+          <TouchableOpacity 
+          style={styles.button}           
+            onPress={() => navigation.navigate('Functions')}
+          >
+          <Text style={styles.buttonText}>Go to functions</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {shouldPlayAfterRecording && isPlaying && 
+      // (
+      //   <Button title="Stop Playback" onPress={stopSound} />
+      // )
+      <View>
+        <TouchableOpacity 
+          style={styles.button}           
+          onPress={stopSound}
+          >
+          <Text style={styles.buttonText}>Stop playback</Text>
+          </TouchableOpacity>
       </View>
-    );
+      }
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#FFF694',
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   button: {
-    backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 5,
+    backgroundColor: '#D67638',
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    marginTop: 20
   },
   buttonText: {
-    color: '#fff',
+    color: 'black',
     fontWeight: 'bold',
-    fontSize: 18,
+    fontSize: 19,
+    textAlign: 'center'
   },
 });
 
